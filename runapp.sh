0c120e03a522e7882bda98dcff1ed9c349fbc941
Chenyao Li <cli@pinterest.com>
2017-10-16 21:47:52 -0700

    updated runapp.sh to start the file serving service in ~/site

--- runapp.sh
+++ runapp.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-( cd ./client ; npm start ) & ( cd ./server/closetapp ; source bin/activate && python app.py)
+( cd ./client ; npm start ) & ( cd ./server/closetapp ; source bin/activate && python app.py) & (cd ~/site ; python -m SimpleHTTPServer 8000)
 
